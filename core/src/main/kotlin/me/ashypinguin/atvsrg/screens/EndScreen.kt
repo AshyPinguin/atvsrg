deeab22d691bc3f73989ffa35b0e6e3e5a4c43a5
Lars <pinguinlars1105@gmail.com>
2026-05-06 14:55:00 +0200

    chore: made the S and SS look PRETTY
    I wanted to do this for so long omg

--- core/src/main/kotlin/me/ashypinguin/atvsrg/screens/EndScreen.kt
+++ core/src/main/kotlin/me/ashypinguin/atvsrg/screens/EndScreen.kt
@@ -3,7 +3,6 @@ package me.ashypinguin.atvsrg.screens
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType.Filled
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType.Line
 import me.ashypinguin.atvsrg.Atvsrg
 import me.ashypinguin.atvsrg.components.clear
 import me.ashypinguin.atvsrg.components.drawableArea
@@ -53,10 +52,15 @@ class EndScreen(game: Atvsrg, @Suppress("CanBeParameter", "RedundantSuppression"
           //Drawing this first so I can override it and so the ss looks stacked
           val lowerSWidth = worldWidth * .125f - worldHeight * .05f
           rect(actualLeftBorder, worldHeight * .25f, lowerSWidth, worldHeight * .1f)
-          rect(actualLeftBorder, worldHeight * .475f, lowerSWidth, worldHeight * .1f)
           rect(actualLeftBorder, worldHeight * .7f, lowerSWidth, worldHeight * .1f)
+          rect(actualLeftBorder, worldHeight * .475f, lowerSWidth, worldHeight * .1f)
           arc(actualLeftBorder, worldHeight * .575f, worldHeight * .1f, 180f, CORNER_DEGREES, ROUNDING_SEGMENTS)
-          rect(actualLeftBorder - worldHeight * .1f, worldHeight * .575f, worldHeight * .1f, worldHeight * .125f)
+          rect(
+            actualLeftBorder - worldHeight * .1f,
+            worldHeight * .575f,
+            worldHeight * (.1f + .125f / 2f),
+            worldHeight * .125f
+          )
           arc(actualLeftBorder, worldHeight * .7f, worldHeight * .1f, 90f, CORNER_DEGREES, ROUNDING_SEGMENTS)
           arc(actualLeftBorder, worldHeight * .3f, worldHeight * .05f, 90f, 180f, ROUNDING_SEGMENTS)
 
@@ -66,62 +70,49 @@ class EndScreen(game: Atvsrg, @Suppress("CanBeParameter", "RedundantSuppression"
           rect(shadowLeftBorder - worldHeight * .1f, worldHeight * .575f, worldHeight * .1f, worldHeight * .125f)
           arc(shadowLeftBorder, worldHeight * .7f, worldHeight * .1f, 90f, CORNER_DEGREES, ROUNDING_SEGMENTS)
           arc(shadowLeftBorder, worldHeight * .3f, worldHeight * .05f, 90f, 180f, ROUNDING_SEGMENTS)
+          arc(
+            actualLeftBorder + worldHeight * (.125f / 2f),
+            worldHeight * (.575f + .125f / 2f),
+            worldHeight * (.125f / 2f),
+            90f, 180f, ROUNDING_SEGMENTS
+          )
 
           color = RANK_SS_COLOR
           rect(leftBorder, worldHeight * .25f, worldWidth * (.825f - RANK_SS_LEFT_BORDER_PERCENT), worldHeight * .1f)
           arc(rightBorder, worldHeight * .35f, worldHeight * .1f, 270f, CORNER_DEGREES, ROUNDING_SEGMENTS)
-          rect(rightBorder, worldHeight * .35f, worldHeight * .1f, worldHeight * .125f)
+          rect(
+            rightBorder - worldHeight * (.125f / 2f),
+            worldHeight * .35f,
+            worldHeight * (.1f + .125f / 2f),
+            worldHeight * .125f
+          )
           arc(rightBorder, worldHeight * .475f, worldHeight * .1f, 0f, CORNER_DEGREES, ROUNDING_SEGMENTS)
           rect(leftBorder, worldHeight * .475f, worldWidth * (.825f - RANK_SS_LEFT_BORDER_PERCENT), worldHeight * .1f)
           arc(leftBorder, worldHeight * .575f, worldHeight * .1f, 180f, CORNER_DEGREES, ROUNDING_SEGMENTS)
-          rect(leftBorder - worldHeight * .1f, worldHeight * .575f, worldHeight * .1f, worldHeight * .125f)
+          rect(
+            leftBorder - worldHeight * .1f,
+            worldHeight * .575f,
+            worldHeight * (.1f + .125f / 2f),
+            worldHeight * .125f
+          )
           arc(leftBorder, worldHeight * .7f, worldHeight * .1f, 90f, CORNER_DEGREES, ROUNDING_SEGMENTS)
           rect(leftBorder, worldHeight * .7f, worldWidth * (.825f - RANK_SS_LEFT_BORDER_PERCENT), worldHeight * .1f)
           arc(rightBorder, worldHeight * .75f, worldHeight * .05f, 270f, 180f, ROUNDING_SEGMENTS)
           arc(leftBorder, worldHeight * .3f, worldHeight * .05f, 90f, 180f, ROUNDING_SEGMENTS)
 
-          end()
-          begin(Line) //fuck you LibGDX
-
-          run { //I don't want to the vars to escape out of this scope :)
-            val r = worldHeight * (.125f / 2f)
-            val cx = leftBorder + r
-            val cy = worldHeight * (.7f - .125f / 2f)
-            val offset = r * 1.3333f
-            curve(
-              cx, cy + r,
-              cx - offset, cy + offset,
-              cx - offset, cy - offset,
-              cx, cy - r,
-              ROUNDING_SEGMENTS
-            )
-          }
-          run { //I don't want to the vars to escape out of this scope :)
-            val r = worldHeight * (.125f / 2f)
-            val cx = rightBorder - r
-            val cy = worldHeight * (.475f - .125f / 2f)
-            val offset = r * 1.3333f
-            curve(
-              cx, cy + r,
-              cx + offset, cy + offset,
-              cx + offset, cy - offset,
-              cx, cy - r,
-              ROUNDING_SEGMENTS
-            )
-          }
-          run { //I don't want to the vars to escape out of this scope :)
-            val r = worldHeight * (.125f / 2f)
-            val cx = actualLeftBorder + r
-            val cy = worldHeight * (.7f - .125f / 2f)
-            val offset = r * 1.3333f
-            curve(
-              cx, cy + r,
-              cx - offset, cy + offset,
-              cx - offset, cy - offset,
-              cx, cy - r,
-              ROUNDING_SEGMENTS
-            )
-          }
+          color = UI_ELEMENT_BG_COLOR
+          arc(
+            rightBorder - worldHeight * (.125f / 2f),
+            worldHeight * (.35f + .125f / 2f),
+            worldHeight * (.125f / 2f),
+            270f, 180f, ROUNDING_SEGMENTS
+          )
+          arc(
+            leftBorder + worldHeight * (.125f / 2f),
+            worldHeight * (.575f + .125f / 2f),
+            worldHeight * (.125f / 2f),
+            90f, 180f, ROUNDING_SEGMENTS
+          )
         }
 
         S -> {
@@ -131,45 +122,39 @@ class EndScreen(game: Atvsrg, @Suppress("CanBeParameter", "RedundantSuppression"
           color = RANK_S_COLOR
           rect(leftBorder, worldHeight * .25f, worldWidth * (.825f - RANK_SS_LEFT_BORDER_PERCENT), worldHeight * .1f)
           arc(rightBorder, worldHeight * .35f, worldHeight * .1f, 270f, CORNER_DEGREES, ROUNDING_SEGMENTS)
-          rect(rightBorder, worldHeight * .35f, worldHeight * .1f, worldHeight * .125f)
+          rect(
+            rightBorder - worldHeight * (.125f / 2f),
+            worldHeight * .35f,
+            worldHeight * (.1f + .125f / 2f),
+            worldHeight * .125f
+          )
           arc(rightBorder, worldHeight * .475f, worldHeight * .1f, 0f, CORNER_DEGREES, ROUNDING_SEGMENTS)
           rect(leftBorder, worldHeight * .475f, worldWidth * (.825f - RANK_SS_LEFT_BORDER_PERCENT), worldHeight * .1f)
           arc(leftBorder, worldHeight * .575f, worldHeight * .1f, 180f, CORNER_DEGREES, ROUNDING_SEGMENTS)
-          rect(leftBorder - worldHeight * .1f, worldHeight * .575f, worldHeight * .1f, worldHeight * .125f)
+          rect(
+            leftBorder - worldHeight * .1f,
+            worldHeight * .575f,
+            worldHeight * (.1f + .125f / 2f),
+            worldHeight * .125f
+          )
           arc(leftBorder, worldHeight * .7f, worldHeight * .1f, 90f, CORNER_DEGREES, ROUNDING_SEGMENTS)
           rect(leftBorder, worldHeight * .7f, worldWidth * (.825f - RANK_SS_LEFT_BORDER_PERCENT), worldHeight * .1f)
           arc(rightBorder, worldHeight * .75f, worldHeight * .05f, 270f, 180f, ROUNDING_SEGMENTS)
           arc(leftBorder, worldHeight * .3f, worldHeight * .05f, 90f, 180f, ROUNDING_SEGMENTS)
 
-          end()
-          begin(Line) //fuck you LibGDX
-
-          run { //I don't want to the vars to escape out of this scope :)
-            val r = worldHeight * (.125f / 2f)
-            val cx = leftBorder + r
-            val cy = worldHeight * (.7f - .125f / 2f)
-            val offset = r * 1.3333f
-            curve(
-              cx, cy + r,
-              cx - offset, cy + offset,
-              cx - offset, cy - offset,
-              cx, cy - r,
-              ROUNDING_SEGMENTS
-            )
-          }
-          run { //I don't want to the vars to escape out of this scope :)
-            val r = worldHeight * (.125f / 2f)
-            val cx = rightBorder - r
-            val cy = worldHeight * (.475f - .125f / 2f)
-            val offset = r * 1.3333f
-            curve(
-              cx, cy + r,
-              cx + offset, cy + offset,
-              cx + offset, cy - offset,
-              cx, cy - r,
-              ROUNDING_SEGMENTS
-            )
-          }
+          color = UI_ELEMENT_BG_COLOR
+          arc(
+            rightBorder - worldHeight * (.125f / 2f),
+            worldHeight * (.35f + .125f / 2f),
+            worldHeight * (.125f / 2f),
+            270f, 180f, ROUNDING_SEGMENTS
+          )
+          arc(
+            leftBorder + worldHeight * (.125f / 2f),
+            worldHeight * (.575f + .125f / 2f),
+            worldHeight * (.125f / 2f),
+            90f, 180f, ROUNDING_SEGMENTS
+          )
         }
 
         A -> {
